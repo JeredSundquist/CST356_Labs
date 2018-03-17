372128399739e5cca8ce567d018d61c2cc901b70
JeredSundquist <jeredjsundquist@hotmail.com>
2018-03-17 15:44:24 -0700

    set up dependency injection: NO injection in controllers yet

---
+++ [CST356] - Lab5/[CST356] - Lab4/App_Start/DependencyInjectionConfig.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;//<---added
+using SimpleInjector;//<---added
+using SimpleInjector.Integration.Web;//<---added
+using SimpleInjector.Integration.Web.Mvc;//<---added
+using _CST356____Lab4.Data;//<---added
+using _CST356____Lab4.Repositories;//<---added
+using System.Reflection;//<---added
+
+namespace _CST356____Lab4.App_Start
+{
+    public static class DependencyInjectionConfig
+    {
+        //ADEN: Used your configuration structure here, though I do see and understand now what is going on
+        public static void Register()
+        {
+            // Create the container as usual.
+            var container = new Container();
+            container.Options.DefaultScopedLifestyle = new WebRequestLifestyle();
+
+            // Register your types, for instance:
+            container.Register<IUserPetRepository, UserPetRepository>(Lifestyle.Scoped);
+            //container.Register<IUserService, UserService>(Lifestyle.Scoped);
+            //container.Register<IPetService, PetService>(Lifestyle.Scoped);
+            container.Register<AppDbContext, AppDbContext>(Lifestyle.Scoped);
+
+            // This is an extension method from the integration package.
+            container.RegisterMvcControllers(Assembly.GetExecutingAssembly());
+
+            container.Verify();
+
+            DependencyResolver.SetResolver(new SimpleInjectorDependencyResolver(container));
+        }
+    }
+}
--- [CST356] - Lab5/[CST356] - Lab4/Global.asax.cs
+++ [CST356] - Lab5/[CST356] - Lab4/Global.asax.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using _CST356____Lab4.App_Start;//<---added
 
 namespace _CST356____Lab4
 {
@@ -16,6 +17,7 @@ namespace _CST356____Lab4
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            DependencyInjectionConfig.Register();//<---Dependency Injection Config File Registered
         }
     }
 }
